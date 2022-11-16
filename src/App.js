48ff8dfae7993d2c67fa49272a78dd26908cf98e
samwpr <swpr10x@gmail.com>
2022-11-16 17:06:13 +0800

    -

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Employee from './components/Employee.js';
+import {useState} from 'react';
 
 function App() {
+  const [role, setRole] = useState("No.1 Lolster");
+  const [employees, setEmployees] = useState(
+    [
+      {name: "Caleb", role: "Dev", img:"https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg"},
+      {name: "Taleb", role: "Dev", img:"https://images.pexels.com/photos/4750165/pexels-photo-4750165.jpeg"},
+      {name: "Aaleb", role: "Dev", img:"https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg"},
+    ]
+  );
+  const showEmployee = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      {showEmployee ? (
+        <div className="App Wrapper flex flex-wrap justify-center">
+          {employees.map((e) => {
+            return(
+            <Employee name={e.name} role={e.role} img={e.img}/>
+            )
+          })}
+        </div>
+
+      ) : <p>No Access</p>}
     </div>
   );
 }
-
 export default App;
+
+
+
+
